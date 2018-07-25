b74ad3142fab1126bd67621f802dcb981d7f7494
Yurina Shin <yurinas@gmail.com>
2018-07-25 11:11:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,10 +26,9 @@ end
 
 def can_be_created_in_a_block(args = {:title => "whatever", :release_date => 1990})
   Movie.create do |m|
-    binding.pry
-    # m.title = args[:title]
-    # m.release_date = args[:release_date]
-    m.can_be_created_with_a_hash_of_attributes
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
