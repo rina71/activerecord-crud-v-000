b728d0ae3740c0dae2ff0d180465eaede00ea6a4
Yurina Shin <yurinas@gmail.com>
2018-07-25 11:34:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -53,7 +53,7 @@ def can_find_by_multiple_attributes
   # release_date == 2000
   # director == "Me"
   binding.pry
-  Movie.find_by(title = "Title", release_date = 2000, director = "Me" )
+  Movie.find_by(:title = "Title", release_date = 2000, director = "Me" )
 end
 
 def can_find_using_where_clause_and_be_sorted
