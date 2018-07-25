a523c0ba399101ed73aa7c07a54b7b49bce4761c
Yurina Shin <yurinas@gmail.com>
2018-07-25 10:40:41 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20180723211926) do
 
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "release_date"
+    t.string   "director"
+    t.string   "lead"
+    t.boolean  "in_theaters"
+  end
+
 end
